e652685f04e822b55313d63885029043b25aa57e
Xednom <monde.lacanlalay@gmail.com>
2021-03-03 20:59:12 +0800

    updated user dropdown for navbar

--- components/Layout/DashboardNavbar.vue
+++ components/Layout/DashboardNavbar.vue
@@ -21,67 +21,11 @@
     </div>
 
     <ul class="navbar-nav" :class="$rtl.isRTL ? 'mr-auto' : 'ml-auto'">
-      <div class="search-bar input-group" @click="searchModalVisible = true">
-        <button
-          class="btn btn-link"
-          id="search-button"
-          data-toggle="modal"
-          data-target="#searchModal"
-        >
-          <i class="tim-icons icon-zoom-split"></i>
-        </button>
-        <!-- You can choose types of search input -->
-      </div>
-      <modal
-        :show.sync="searchModalVisible"
-        class="modal-search"
-        id="searchModal"
-        :centered="false"
-        :show-close="true"
-      >
-        <input
-          slot="header"
-          v-model="searchQuery"
-          type="text"
-          class="form-control"
-          id="inlineFormInputGroup"
-          placeholder="SEARCH"
-        />
-      </modal>
-      <base-dropdown
-        tag="li"
-        :menu-on-right="!$rtl.isRTL"
-        title-tag="a"
-        title-classes="nav-link"
-        class="nav-item"
-      >
-        <template
-          slot="title"
-        >
-          <div class="notification d-none d-lg-block d-xl-block"></div>
-          <i class="tim-icons icon-sound-wave"></i>
-          <p class="d-lg-none">New Notifications</p>
-        </template>
-        <li class="nav-link">
-          <a href="#" class="nav-item dropdown-item"
-            >Mike John responded to your email</a
-          >
-        </li>
-        <li class="nav-link">
-          <a href="#" class="nav-item dropdown-item">You have 5 more tasks</a>
-        </li>
-        <li class="nav-link">
-          <a href="#" class="nav-item dropdown-item"
-            >Your friend Michael is in town</a
-          >
-        </li>
-        <li class="nav-link">
-          <a href="#" class="nav-item dropdown-item">Another notification</a>
-        </li>
-        <li class="nav-link">
-          <a href="#" class="nav-item dropdown-item">Another one</a>
-        </li>
-      </base-dropdown>
+      <li class="current-user">
+        <span>
+          {{ this.$auth.user.first_name }} {{ this.$auth.user.last_name }}
+        </span>
+      </li>
       <base-dropdown
         tag="li"
         :menu-on-right="!$rtl.isRTL"
@@ -90,18 +34,15 @@
         title-classes="nav-link"
         menu-classes="dropdown-navbar"
       >
-        <template
-          slot="title"
-        >
+        <template slot="title">
           <div class="photo"><img src="img/mike.jpg" /></div>
           <b class="caret d-none d-lg-block d-xl-block"></b>
           <p class="d-lg-none">Log out</p>
         </template>
         <li class="nav-link">
-          <a href="#" class="nav-item dropdown-item">Profile</a>
-        </li>
-        <li class="nav-link">
-          <a href="#" class="nav-item dropdown-item">Settings</a>
+          <nuxt-link class="nav-item dropdown-item" to="/pages/user"
+            >Profile</nuxt-link
+          >
         </li>
         <div class="dropdown-divider"></div>
         <li class="nav-link">
@@ -112,39 +53,39 @@
   </base-nav>
 </template>
 <script>
-import { CollapseTransition } from 'vue2-transitions';
-import { BaseNav, Modal } from '@/components';
-import SidebarToggleButton from './SidebarToggleButton';
+import { CollapseTransition } from "vue2-transitions";
+import { BaseNav, Modal } from "@/components";
+import SidebarToggleButton from "./SidebarToggleButton";
 
 export default {
   components: {
     SidebarToggleButton,
     CollapseTransition,
     BaseNav,
-    Modal
+    Modal,
   },
   computed: {
     routeName() {
       const { path } = this.$route;
-      let parts = path.split('/')
-      return parts.map(p => this.capitalizeFirstLetter(p)).join(' ');
+      let parts = path.split("/");
+      return parts.map((p) => this.capitalizeFirstLetter(p)).join(" ");
     },
     isRTL() {
       return this.$rtl.isRTL;
-    }
+    },
   },
   data() {
     return {
       activeNotifications: false,
       showMenu: false,
       searchModalVisible: false,
-      searchQuery: ''
+      searchQuery: "",
     };
   },
   methods: {
     capitalizeFirstLetter(string) {
-      if (!string || typeof string !== 'string') {
-        return ''
+      if (!string || typeof string !== "string") {
+        return "";
       }
       return string.charAt(0).toUpperCase() + string.slice(1);
     },
@@ -158,15 +99,18 @@ export default {
       this.showMenu = !this.showMenu;
     },
     async logout() {
-			await this.$auth.logout().then(res => {
-				this.$router.push("/login")
-			});
-		}
-  }
+      await this.$auth.logout().then((res) => {
+        this.$router.push("/login");
+      });
+    },
+  },
 };
 </script>
 <style scoped>
 .top-navbar {
   top: 0px;
+}
+.navbar-nav .current-user {
+  padding-top: 10px !important;
 }
 </style>
