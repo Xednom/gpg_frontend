bc861d596e633e738c23a4dc2ca1b7e40bf489c4
Xednom <monde.lacanlalay@gmail.com>
2021-03-05 13:27:17 +0800

    updated the content footer

--- components/Layout/ContentFooter.vue
+++ components/Layout/ContentFooter.vue
@@ -1,38 +1,6 @@
 <template>
   <footer class="footer">
     <div class="container-fluid">
-      <ul class="nav">
-        <li class="nav-item">
-          <a
-            href="http://creative-tim.com"
-            target="_blank"
-            rel="noopener"
-            class="nav-link"
-          >
-            Creative Tim
-          </a>
-        </li>
-        <li class="nav-item">
-          <a
-            href="https://www.creative-tim.com/presentation"
-            target="_blank"
-            rel="noopener"
-            class="nav-link"
-          >
-            About Us
-          </a>
-        </li>
-        <li class="nav-item">
-          <a
-            href="http://blog.creative-tim.com"
-            target="_blank"
-            rel="noopener"
-            class="nav-link"
-          >
-            Blog
-          </a>
-        </li>
-      </ul>
       <div class="copyright">
         &copy; GPG Corporation {{ year }}, made with <i class="tim-icons icon-heart-2"></i> by
         <a href="https://psalmsglobal.com/" rel="noopener" target="_blank">Psalms Global</a>
