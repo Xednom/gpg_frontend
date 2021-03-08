24bf4e9114a417a46d9d481e70eeec3f59de078f
Xednom <monde.lacanlalay@gmail.com>
2021-03-08 14:34:02 +0800

    added Job Order General UIs

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,39 +12,34 @@
           :link="{
             name: $t('sidebar.dashboard'),
             icon: 'tim-icons icon-chart-pie-36',
-            path: '/'
+            path: '/',
           }"
         >
         </sidebar-item>
         <sidebar-item
-          :link="{ name: $t('sidebar.pages'), icon: 'tim-icons icon-image-02' }"
+          :link="{
+            name: $t('Job order'),
+            icon: 'tim-icons icon-bullet-list-67',
+            path: '/',
+          }"
         >
           <sidebar-item
-            :link="{ name: $t('sidebar.pricing'), path: '/pricing' }"
+            :link="{
+              name: 'General',
+              path: '/job-order/general',
+            }"
           ></sidebar-item>
           <sidebar-item
-            :link="{ name: $t('sidebar.rtl'), path: localePath('pages-rtl', 'ar') }"
-          ></sidebar-item>
-          <sidebar-item
-            :link="{ name: $t('sidebar.timeline'), path: '/pages/timeline' }"
-          ></sidebar-item>
-          <sidebar-item
-            :link="{ name: $t('sidebar.login'), path: '/login' }"
-          ></sidebar-item>
-          <sidebar-item
-            :link="{ name: $t('sidebar.register'), path: '/register' }"
-          ></sidebar-item>
-          <sidebar-item
-            :link="{ name: $t('sidebar.lock'), path: '/lock' }"
-          ></sidebar-item>
-          <sidebar-item
-            :link="{ name: $t('sidebar.userProfile'), path: '/pages/user' }"
+            :link="{
+              name: 'Land Master JO by APN',
+              path: '/job-order/apn',
+            }"
           ></sidebar-item>
         </sidebar-item>
         <sidebar-item
           :link="{
             name: $t('sidebar.components'),
-            icon: 'tim-icons icon-molecule-40'
+            icon: 'tim-icons icon-molecule-40',
           }"
         >
           <sidebar-item :link="{ name: $t('sidebar.multiLevelCollapse') }">
@@ -53,7 +48,7 @@
                 name: $t('sidebar.example'),
                 isRoute: false,
                 path: 'https://google.com',
-                target: '_blank'
+                target: '_blank',
               }"
             ></sidebar-item>
           </sidebar-item>
@@ -64,7 +59,7 @@
           <sidebar-item
             :link="{
               name: $t('sidebar.gridSystem'),
-              path: '/components/grid-system'
+              path: '/components/grid-system',
             }"
           ></sidebar-item>
           <sidebar-item
@@ -73,13 +68,13 @@
           <sidebar-item
             :link="{
               name: $t('sidebar.sweetAlert'),
-              path: '/components/sweet-alert'
+              path: '/components/sweet-alert',
             }"
           ></sidebar-item>
           <sidebar-item
             :link="{
               name: $t('sidebar.notifications'),
-              path: '/components/notifications'
+              path: '/components/notifications',
             }"
           ></sidebar-item>
           <sidebar-item
@@ -88,7 +83,7 @@
           <sidebar-item
             :link="{
               name: $t('sidebar.typography'),
-              path: '/components/typography'
+              path: '/components/typography',
             }"
           ></sidebar-item>
         </sidebar-item>
@@ -101,13 +96,13 @@
           <sidebar-item
             :link="{
               name: $t('sidebar.extendedForms'),
-              path: '/forms/extended'
+              path: '/forms/extended',
             }"
           ></sidebar-item>
           <sidebar-item
             :link="{
               name: $t('sidebar.validationForms'),
-              path: '/forms/validation'
+              path: '/forms/validation',
             }"
           ></sidebar-item>
           <sidebar-item
@@ -117,77 +112,37 @@
         <sidebar-item
           :link="{
             name: $t('sidebar.tables'),
-            icon: 'tim-icons icon-puzzle-10'
+            icon: 'tim-icons icon-puzzle-10',
           }"
         >
           <sidebar-item
             :link="{
               name: $t('sidebar.regularTables'),
-              path: '/table-list/regular'
+              path: '/table-list/regular',
             }"
           ></sidebar-item>
           <sidebar-item
             :link="{
               name: $t('sidebar.extendedTables'),
-              path: '/table-list/extended'
+              path: '/table-list/extended',
             }"
           ></sidebar-item>
           <sidebar-item
             :link="{
               name: $t('sidebar.paginatedTables'),
-              path: '/table-list/paginated'
+              path: '/table-list/paginated',
             }"
           ></sidebar-item>
         </sidebar-item>
-        <sidebar-item
-          :link="{ name: $t('sidebar.maps'), icon: 'tim-icons icon-pin' }"
-        >
-          <sidebar-item
-            :link="{ name: $t('sidebar.googleMaps'), path: '/maps/google' }"
-          ></sidebar-item>
-          <sidebar-item
-            :link="{
-              name: $t('sidebar.fullScreenMaps'),
-              path: '/maps/full-screen'
-            }"
-          ></sidebar-item>
-          <sidebar-item
-            :link="{ name: $t('sidebar.vectorMaps'), path: '/maps/vector-map' }"
-          ></sidebar-item>
-        </sidebar-item>
-        <sidebar-item
-          :link="{
-            name: $t('sidebar.widgets'),
-            icon: 'tim-icons icon-settings',
-            path: '/widgets'
-          }"
-        ></sidebar-item>
-        <sidebar-item
-          :link="{
-            name: $t('sidebar.charts'),
-            icon: 'tim-icons icon-chart-bar-32',
-            path: '/charts'
-          }"
-        ></sidebar-item>
-        <sidebar-item
-          :link="{
-            name: $t('sidebar.calendar'),
-            icon: 'tim-icons icon-time-alarm',
-            path: '/calendar'
-          }"
-        ></sidebar-item>
       </template>
     </side-bar>
     <!--Share plugin (for demo purposes). You can remove it if don't plan on using it-->
-    <sidebar-share :background-color.sync="sidebarBackground"> </sidebar-share>
+    <!-- <sidebar-share :background-color.sync="sidebarBackground"> </sidebar-share> -->
     <div class="main-panel" :data="sidebarBackground">
       <dashboard-navbar></dashboard-navbar>
       <router-view name="header"></router-view>
 
-      <div
-        :class="{ content: !isFullScreenRoute }"
-        @click="toggleSidebar"
-      >
+      <div :class="{ content: !isFullScreenRoute }" @click="toggleSidebar">
         <zoom-center-transition :duration="200" mode="out-in">
           <!-- your content here -->
           <nuxt></nuxt>
@@ -198,105 +153,105 @@
   </div>
 </template>
 <script>
-  /* eslint-disable no-new */
-  import PerfectScrollbar from 'perfect-scrollbar';
-  import 'perfect-scrollbar/css/perfect-scrollbar.css';
-  import SidebarShare from '@/components/Layout/SidebarSharePlugin';
-  function hasElement(className) {
-    return document.getElementsByClassName(className).length > 0;
+/* eslint-disable no-new */
+import PerfectScrollbar from "perfect-scrollbar";
+import "perfect-scrollbar/css/perfect-scrollbar.css";
+import SidebarShare from "@/components/Layout/SidebarSharePlugin";
+function hasElement(className) {
+  return document.getElementsByClassName(className).length > 0;
+}
+
+function initScrollbar(className) {
+  if (hasElement(className)) {
+    new PerfectScrollbar(`.${className}`);
+  } else {
+    // try to init it later in case this component is loaded async
+    setTimeout(() => {
+      initScrollbar(className);
+    }, 100);
   }
+}
 
-  function initScrollbar(className) {
-    if (hasElement(className)) {
-      new PerfectScrollbar(`.${className}`);
-    } else {
-      // try to init it later in case this component is loaded async
-      setTimeout(() => {
-        initScrollbar(className);
-      }, 100);
-    }
-  }
+import DashboardNavbar from "@/components/Layout/DashboardNavbar.vue";
+import ContentFooter from "@/components/Layout/ContentFooter.vue";
+import DashboardContent from "@/components/Layout/Content.vue";
+import SidebarFixedToggleButton from "@/components/Layout/SidebarFixedToggleButton.vue";
+import { SlideYDownTransition, ZoomCenterTransition } from "vue2-transitions";
 
-  import DashboardNavbar from '@/components/Layout/DashboardNavbar.vue';
-  import ContentFooter from '@/components/Layout/ContentFooter.vue';
-  import DashboardContent from '@/components/Layout/Content.vue';
-  import SidebarFixedToggleButton from '@/components/Layout/SidebarFixedToggleButton.vue';
-  import { SlideYDownTransition, ZoomCenterTransition } from 'vue2-transitions';
-
-  export default {
-    middleware: ['auth'],
-    components: {
-      DashboardNavbar,
-      ContentFooter,
-      SidebarFixedToggleButton,
-      DashboardContent,
-      SlideYDownTransition,
-      ZoomCenterTransition,
-      SidebarShare
+export default {
+  middleware: ["auth"],
+  components: {
+    DashboardNavbar,
+    ContentFooter,
+    SidebarFixedToggleButton,
+    DashboardContent,
+    SlideYDownTransition,
+    ZoomCenterTransition,
+    SidebarShare,
+  },
+  data() {
+    return {
+      sidebarBackground: "vue", //vue|blue|orange|green|red|primary
+    };
+  },
+  computed: {
+    isFullScreenRoute() {
+      return this.$route.path === "/maps/full-screen";
     },
-    data() {
-      return {
-        sidebarBackground: 'vue' //vue|blue|orange|green|red|primary
-      };
-    },
-    computed: {
-      isFullScreenRoute() {
-        return this.$route.path === '/maps/full-screen'
+  },
+  methods: {
+    toggleSidebar() {
+      if (this.$sidebar.showSidebar) {
+        this.$sidebar.displaySidebar(false);
       }
     },
-    methods: {
-      toggleSidebar() {
-        if (this.$sidebar.showSidebar) {
-          this.$sidebar.displaySidebar(false);
-        }
-      },
-      initScrollbar() {
-        let docClasses = document.body.classList;
-        let isWindows = navigator.platform.startsWith('Win');
-        if (isWindows) {
-          // if we are on windows OS we activate the perfectScrollbar function
-          initScrollbar('sidebar');
-          initScrollbar('main-panel');
-          initScrollbar('sidebar-wrapper');
+    initScrollbar() {
+      let docClasses = document.body.classList;
+      let isWindows = navigator.platform.startsWith("Win");
+      if (isWindows) {
+        // if we are on windows OS we activate the perfectScrollbar function
+        initScrollbar("sidebar");
+        initScrollbar("main-panel");
+        initScrollbar("sidebar-wrapper");
 
-          docClasses.add('perfect-scrollbar-on');
-        } else {
-          docClasses.add('perfect-scrollbar-off');
-        }
+        docClasses.add("perfect-scrollbar-on");
+      } else {
+        docClasses.add("perfect-scrollbar-off");
       }
     },
-    mounted() {
-      this.initScrollbar();
-    }
-  };
+  },
+  mounted() {
+    this.initScrollbar();
+  },
+};
 </script>
 <style lang="scss">
-  $scaleSize: 0.95;
-  @keyframes zoomIn95 {
-    from {
-      opacity: 0;
-      transform: scale3d($scaleSize, $scaleSize, $scaleSize);
-    }
-    to {
-      opacity: 1;
-    }
+$scaleSize: 0.95;
+@keyframes zoomIn95 {
+  from {
+    opacity: 0;
+    transform: scale3d($scaleSize, $scaleSize, $scaleSize);
   }
+  to {
+    opacity: 1;
+  }
+}
 
-  .main-panel .zoomIn {
-    animation-name: zoomIn95;
-  }
+.main-panel .zoomIn {
+  animation-name: zoomIn95;
+}
 
-  @keyframes zoomOut95 {
-    from {
-      opacity: 1;
-    }
-    to {
-      opacity: 0;
-      transform: scale3d($scaleSize, $scaleSize, $scaleSize);
-    }
+@keyframes zoomOut95 {
+  from {
+    opacity: 1;
   }
+  to {
+    opacity: 0;
+    transform: scale3d($scaleSize, $scaleSize, $scaleSize);
+  }
+}
 
-  .main-panel .zoomOut {
-    animation-name: zoomOut95;
-  }
+.main-panel .zoomOut {
+  animation-name: zoomOut95;
+}
 </style>
