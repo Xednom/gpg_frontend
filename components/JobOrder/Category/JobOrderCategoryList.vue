b16477c1c52d3c447f824a44f7e21593d740f26a
Xednom <monde.lacanlalay@gmail.com>
2021-04-19 20:32:16 +0800

    fixed url in JO category

--- components/JobOrder/Category/JobOrderCategoryList.vue
+++ components/JobOrder/Category/JobOrderCategoryList.vue
@@ -109,7 +109,7 @@
                 </template>
 
                 <template #cell(url_of_the_completed_jo)="row">
-                  <a :href="'//' + row.item.url_of_the_completed_jo" target="_blank">link here</a>
+                  <a :href="row.item.url_of_the_completed_jo" target="_blank">link here</a>
                 </template>
               </b-table>
 
