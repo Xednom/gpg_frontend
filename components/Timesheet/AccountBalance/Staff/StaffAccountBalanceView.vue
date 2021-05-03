9e884cb46016fe27701bf67055faf4beb7d15c17
Xednom <monde.lacanlalay@gmail.com>
2021-05-03 20:47:54 +0800

    added payment history and account balance for staff

---
+++ components/Timesheet/AccountBalance/Staff/StaffAccountBalanceView.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="col-md-12 white-content">
+    <form @submit.prevent="save">
+      <h4 class="card-title">
+        Account Balance of Transaction #<b>{{ balance.id }}</b>
+      </h4>
+      <div class="form-row">
+        <div class="col-sm-12 col-md-6">
+          <base-input v-model="balance.date" label="Date" disabled>
+          </base-input>
+        </div>
+        <div class="col-sm-12 col-md-6">
+          <base-input label="Amount Due" v-model="balance.amount_due_w_currency" disabled>
+          </base-input>
+        </div>
+      </div>
+      <div class="form-row">
+          <div class="col-sm-12 col-md-6">
+              <base-input label="Payment Made" v-model="balance.payment_made_w_currency" disabled>
+          </base-input>
+          </div>
+          <div class="col-sm-12 col-md-6">
+              <base-input label="Account balance" v-model="balance.account_balance_w_currency" disabled>
+          </base-input>
+          </div>
+      </div>
+
+      <div class="form-row">
+        <div class="col-sm-12 col-md-12">
+          <label>Notes</label>
+          <textarea
+            class="form-control"
+            v-model="balance.notes"
+            disabled
+          >
+          </textarea>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import { DatePicker } from "element-ui";
+
+export default {
+  components: {
+    [DatePicker.name]: DatePicker,
+  },
+  props: {
+    balance: {
+      type: Object,
+      decription: "Staff Account Balance data",
+    },
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
---
+++ pages/timesheet/staff-account-balance/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <account-balance-list></account-balance-list>
+</template>
+
+<script>
+import AccountBalanceList from "@/components/Timesheet/AccountBalance/Staff/StaffAccountBalanceList";
+export default {
+  components: {
+    AccountBalanceList,
+  },
+};
+</script>
