67cf667641bd54a1fc6964927ab3aa9d6a88edd2
Xednom <monde.lacanlalay@gmail.com>
2021-05-13 16:07:07 +0800

    added phoneline extension

---
+++ pages/due-diligence/phone-line-extension/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <phone-line></phone-line>
+</template>
+
+<script>
+import PhoneLine from "@/components/PhoneLine/PhoneListList";
+export default {
+    components: {
+        PhoneLine
+    }
+}
+</script>
