89b6cd98119bc79c8ce32848fdcf8407d9ae2b93
Xednom <monde.lacanlalay@gmail.com>
2021-07-28 17:08:47 +0800

    edited the revised terms and condition

--- pages/terms-and-condition/index.vue
+++ pages/terms-and-condition/index.vue
@@ -75,46 +75,47 @@
 
           <p>
             This Agreement shall begin on the date hereof. Parts of this website
-            offer an opportunity for users to post and exchange opinions and
-            information in certain areas of the website. G.P.G Management
-            Consulting and Research Center Corp does not filter, edit, publish
-            or review Comments prior to their presence on the website. Comments
-            do not reflect the views and opinions of G.P.G Management Consulting
-            and Research Center Corp,its agents and/or affiliates. Comments
-            reflect the views and opinions of the person who post their views
-            and opinions. To the extent permitted by applicable laws, G.P.G
-            Management Consulting and Research Center Corp shall not be liable
-            for the Comments or for any liability, damages or expenses caused
-            and/or suffered as a result of any use of and/or posting of and/or
-            appearance of the Comments on this website.
+            offer an opportunity for users to post and exchange and information
+            in certain areas of the website. G.P.G Management Consulting and
+            Research Center Corp does not filter, edit, publish or review
+            Information prior to their presence on the website. Comments do not
+            reflect the views and opinions of G.P.G Management Consulting and
+            Research Center Corp, its agents and/or affiliates. Comments reflect
+            the views and information of the person who post their information.
+            To the extent permitted by applicable laws, G.P.G Management
+            Consulting and Research Center Corp shall not be liable for the
+            Comments or for any liability, damages or expenses caused and/or
+            suffered as a result of any use of and/or posting of and/or
+            appearance of the information on this website.
           </p>
 
           <p>
             G.P.G Management Consulting and Research Center Corp reserves the
-            right to monitor all Comments and to remove any Comments which can
-            be considered inappropriate, offensive or causes breach of these
-            Terms and Conditions.
+            right to monitor all Information and to remove any Information, or
+            Comments in certain information which can be considered
+            inappropriate, offensive or causes breach of these Terms and
+            Conditions.
           </p>
 
           <p>You warrant and represent that:</p>
 
           <ul>
             <li>
-              You are entitled to post the Comments on our website and have all
+              You are entitled to post the Information on our website and have all
               necessary licenses and consents to do so;
             </li>
             <li>
-              The Comments do not invade any intellectual property right,
+              The Information do not invade any intellectual property right,
               including without limitation copyright, patent or trademark of any
               third party;
             </li>
             <li>
-              The Comments do not contain any defamatory, libelous, offensive,
+              The Information do not contain any defamatory, libelous, offensive,
               indecent or otherwise unlawful material which is an invasion of
               privacy
             </li>
             <li>
-              The Comments will not be used to solicit or promote business or
+              The Information will not be used to solicit or promote business or
               custom or present commercial activities or unlawful activity.
             </li>
           </ul>
@@ -255,9 +256,9 @@
             We reserve the right to request that you remove all links or any
             particular link to our Website. You approve to immediately remove
             all links to our Website upon request. We also reserve the right to
-            amen these terms and conditions and it’s linking policy at any time.
-            By continuously linking to our Website, you agree to be bound to and
-            follow these linking terms and conditions.
+            amend these terms and conditions and it’s linking policy at any
+            time. By continuously linking to our Website, you agree to be bound
+            to and follow these linking terms and conditions.
           </p>
 
           <h3><strong>Removal of links from our website</strong></h3>
@@ -267,13 +268,6 @@
             reason, you are free to contact and inform us any moment. We will
             consider requests to remove links but we are not obligated to or so
             or to respond to you directly.
-          </p>
-
-          <p>
-            We do not ensure that the information on this website is correct, we
-            do not warrant its completeness or accuracy; nor do we promise to
-            ensure that the website remains available or that the material on
-            the website is kept up to date.
           </p>
 
           <h3><strong>Disclaimer</strong></h3>
